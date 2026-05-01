9c6185d6e61e7662e8a86f7af1d0c75b23b6b0e3
Simeon Lombard <lombard02simeon@gmail.com>
2026-05-01 14:45:43 +0200

    Initial push of facility management mobile frontend

---
+++ Mobile/android/app/src/main/kotlin/za/ac/akademia/fbs_toep/MainActivity.kt
@@ -0,0 +1,5 @@
+package za.ac.akademia.fbs_toep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Mobile/lib/core/constants/app_strings.dart
@@ -0,0 +1,4 @@
+class AppStrings {
+  static const String staffDomain = 'akademia.ac.za';
+  static const String studentDomain = 'akademiastudente.co.za';
+}
