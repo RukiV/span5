7e887a473b593e233462f02ff627c970319b7a57
Ayden Walker <2024040789@akademiastudente.co.za>
2026-06-03 15:26:30 +0200

    basic login

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -85,7 +85,7 @@ export const workOrdersAPI = {
 
 // Auth API
 export const authAPI = {
-  login: (username, password) => apiClient.post('/auth/login', { username, password }),
+  login: (user_email, user_password) => apiClient.post('/auth/login', { user_email, user_password }),
   me: () => apiClient.get('/auth/me'),
   logout: () => apiClient.post('/auth/logout')
 };
