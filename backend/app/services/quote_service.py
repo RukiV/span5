a54c4fe79ae0fc423761e77b0d44e86ea2e0dad0
Ayden Walker <2024040789@akademiastudente.co.za>
2026-06-27 15:20:57 +0200

    Minor login fix

--- backend/app/services/quote_service.py
+++ backend/app/services/quote_service.py
@@ -1,4 +1,4 @@
 from ..models.quote import Quote, QuoteCreate, QuoteUpdate
 from .base_service import BaseService
 
-fault_service = BaseService[Quote, QuoteCreate, QuoteUpdate](Quote)
+quote_service = BaseService[Quote, QuoteCreate, QuoteUpdate](Quote)
