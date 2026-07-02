afc7502fe4e08cd6ce98ba383c9aee487480a657
V <2024080203@akademiastudente.co.za>
2026-07-02 16:30:36 +0200

    Analytics beta

--- backend/app/services/job_service.py
+++ backend/app/services/job_service.py
@@ -1,17 +1,4 @@
-from sqlmodel import Session, select
-
 from ..models.job import Jobcard, JobcardCreate, JobcardUpdate
 from .base_service import BaseService
 
-class JobService(BaseService[Jobcard, JobcardCreate, JobcardUpdate]):
-    def __init__(self):
-        super().__init__(Jobcard)
-
-    def getRecent(self, session: Session, limit: int = 5):
-        return session.exec(
-            select(Jobcard)
-            .order_by(Jobcard.job_createddatetime.desc().nullslast())
-            .limit(limit)
-        ).all()
-
-job_service = JobService()
+job_service = BaseService[Jobcard, JobcardCreate, JobcardUpdate](Jobcard)
