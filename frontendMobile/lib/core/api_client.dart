36da3a10d447a84a19564ee4a1a56ef33e2f4038
Simeon Lombard <lombard02simeon@gmail.com>
2026-07-05 09:49:25 +0200

    My frontend code,groep backend code (Merge Conflict Resolution)

--- frontendMobile/lib/core/api_client.dart
+++ frontendMobile/lib/core/api_client.dart
@@ -1,4 +1,4 @@
-import 'package:dio/dio.dart';
+import  'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
