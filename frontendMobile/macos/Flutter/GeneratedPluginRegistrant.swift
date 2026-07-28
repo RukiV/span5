0d6f144852f7f80c4e1146f1ad2d0f80976a57e2
Jake Kruger <2024040577@akademiastudente.co.za>
2026-07-28 10:23:51 +0200

    analytics, ui

--- frontendMobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontendMobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,20 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import file_selector_macos
-import flutter_secure_storage_macos
-import geolocator_apple
-import local_auth_darwin
-import mobile_scanner
-import shared_preferences_foundation
-import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
-  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
